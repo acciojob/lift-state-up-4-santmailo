cf4ef32122425e2f314c6f9b5ad92f0b8561e2de
santmailo <iamsunil006@gmail.com>
2023-09-01 17:41:42 +0530

    done assighment lift state up 4

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,68 @@
 
 import React from "react";
-import Parent from "./parent";
 import './../styles/App.css';
+
+const Parent = () => {
+  let [cartitems, setCartitems] = React.useState([]);
+
+  const addItem = (e) => {
+    e.preventDefault();
+    const item = e.target.itemName.value;
+    const price = e.target.itemPrice.value;
+    console.log(item,price);
+    setCartitems([...cartitems, {item, price}]);
+  }
+
+
+  const deleteItem = (index) => {
+    const newCartItems = [...cartitems];
+    newCartItems.splice(index, 1);
+    setCartitems(newCartItems);
+  }
+
+  return (
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <form onSubmit={addItem}>
+        <label htmlFor="itemName">Item Name</label> 
+        <input type = "text" id="itemName" placeholder="Item name" />
+        &nbsp;
+        &nbsp;
+        &nbsp;
+        <label htmlFor="itemPrice">Item Price</label>
+        <input type="number" id="itemPrice" placeholder="Item Price" />
+        &nbsp;
+        &nbsp;
+        <button>Add Item</button>
+      </form>
+      <Child cartitems={cartitems} deleteItem={deleteItem} />
+    </div>
+  )
+}
+
+const Child = ({cartitems, deleteItem}) => {
+  return (
+    <div className="child">
+      <h1>Child component</h1>
+      <ul className="cart-items">
+        {cartitems.map((item,index) => (
+          <li key={index+1}>
+            <span>{item.item}</span>  &nbsp;  &nbsp;  &nbsp;-  &nbsp;  &nbsp;  &nbsp;
+            <span>{"$"+item.price}</span> &nbsp;  &nbsp;  &nbsp;-  &nbsp;  &nbsp;  &nbsp;
+            <button onClick={() => deleteItem(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+
+
+
+
+
+
 
 const App = () => {
   return (
